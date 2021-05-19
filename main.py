a79ef354959dc7a2d818a06ce1aa133e2a3440f0
RyderRetzlaff <>
2021-05-19 04:52:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def password(prompt="Password: ", mask="*"):
+  import stdiomask as passwd
+  import base64
+  password_ = passwd.getpass(prompt, mask)
+  password_ = password_.encode("utf-8")
+  encoded = base64.b64encode(password_)
+  encoded = base64.b64encode(encoded)
+  return encoded.decode("utf-8")
+def decode(encoded):
+  import base64
+  encoded = encoded.encode("utf-8")
+  passwd = base64.b64decode(encoded)
+  passwd = base64.b64decode(passwd)
+  return passwd.decode("utf-8")
+passwd = password()
+print(passwd)
+print("decoded")
+print(decode(passwd))
